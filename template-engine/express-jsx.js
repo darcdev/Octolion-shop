ca9285ccc6834f2b71a8324b2352a7c33e5a54e1
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-12 17:08:35 -0500

    create template engine basic example

---
+++ template-engine/express-jsx.js
@@ -0,0 +1,23 @@
+const fs  = require('fs');
+function getkeysFromOptions(options){
+    const {settings , _locals , ...objectKeys} = options;
+    return Object.keys(objectKeys);
+}
+function getRenderedContent(content, options){
+    const keys = getkeysFromOptions(options);
+    let contentString = content.toString();
+    for(let key of keys) {
+        contentString = contentString.replace(new RegExp(`\{$key\}`,"gi"),options[key])
+    }
+    return contentString;
+}
+function expressJsx(filepath , options , callback){
+    fs.readFile(filepath , (err,content) => {
+        if(err){
+            return callback(err);
+        }
+        const rendered = getRenderedContent(content,options);
+        return callback(null, rendered);
+    })
+}
+module.exports = expressJsx
---
+++ template-engine/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const expressJsx = require('./express-jsx');
+
+app.engine('jsx' , expressJsx);
+app.set("views", "./views");
+app.set("view engine", "jsx");
+
+
+app.get('/', (req, res) => {
+    res.render("index", { hello: 'hola', "world": "mundo" })
+})
+
+const server = app.listen(3000, () => {
+    console.log(`Listening on port ${server.address().port}`);
+})
