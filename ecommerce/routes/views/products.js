d25598c992b294c8019bc9027e57b35b17e914ce
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-12 22:54:02 -0500

    fix details legibility

--- ecommerce/routes/views/products.js
+++ ecommerce/routes/views/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const productsService = require('../services/products');
+const productsService = require('../../services/products');
 
 router.get('/' ,async (req,res,next) => {
     const { tags } = req.query
